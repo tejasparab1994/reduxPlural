1282f987beaf931e1398b016a7a86ba9892543f1
Tejas Parab <tejasparab890@gmail.com>
2018-08-17 16:31:22 -0400

    store added

---
+++ src/store/configureStore.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+
+
+// useful to set initial state, especially while doing server
+// side rendering
+// redux devtool extension added here only
+
+export default function configureStore(initialState) {
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(reduxImmutableStateInvariant())
+  );
+}
