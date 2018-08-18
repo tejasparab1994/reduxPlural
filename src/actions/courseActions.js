dcc858f892f30b751e80be9763c4e4fb7e02f3c4
Tejas Parab <tejasparab890@gmail.com>
2018-08-18 15:02:06 -0400

    LOAD COURSES SUCCESS ACTION CREATED

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,19 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course };
+export function loadCoursesSuccess(course) {
+  return { type: types.LOAD_COURSE_SUCCESS, course };
+}
+
+// a thunk always returns a function that excepts a dispatch
+export function loadCourses() {
+  return function dispatch() {
+    return courseApi.getAllCourses()
+    .then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    })
+    .catch(error => {
+      throw(error);
+    });
+  }
 }
