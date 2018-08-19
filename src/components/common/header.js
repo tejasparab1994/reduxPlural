33bdb63289de1e730b0fd0c8d83c13412061c1be
Tejas Parab <tejasparab890@gmail.com>
2018-08-18 23:01:01 -0400

    loading dots animation added for ajax calls

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,6 +1,6 @@
 import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
-
+import LoadingDots from './LoadingDots';
 
 const Header = () => {
   return (
@@ -10,6 +10,7 @@ const Header = () => {
       <Link to="/about" activeClassName="active">About</Link>
       {" | "}
       <Link to="/courses" activeClassName="active">Courses</Link>
+      <LoadingDots interval={100} dots={20}/>
     </nav>
   );
 };
